c3c7be366a07ded22ed0431c09a32aa5954bff95
Eno <zeroenoch@gmail.com>
2024-11-10 14:50:16 +0800

    refactor create a new api client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,15 +1,29 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
     count: number;
     results: T[];
 }
 
-const apiClient = axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params:{
-        key: 'bd5d6dc4be894636acfcd6febca96079'
-    }
-})
+const axiosInstance = axios.create({
+    baseURL: "https://api.rawg.io/api",
+    params: {
+        key: "bd5d6dc4be894636acfcd6febca96079",
+    },
+});
 
-export default apiClient
+class APIClient<T> {
+    endpoint: string;
+
+    constructor(endpoint: string) {
+        this.endpoint = endpoint;
+    }
+
+    getALL = (config: AxiosRequestConfig) => {
+        return axiosInstance
+            .get<FetchResponse<T>>(this.endpoint, config)
+            .then((res) => res.data);
+    };
+}
+
+export default APIClient;
